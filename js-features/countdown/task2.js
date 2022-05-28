78b886ee312f3bb2e14b2f464e66c82bd6731dcc
VladimsKov <vladimskv@yandex.ru>
2022-05-28 23:50:57 +0300

    Решение

--- js-features/countdown/task2.js
+++ js-features/countdown/task2.js
@@ -1,10 +1,33 @@
 const timer = document.getElementById("timer");
-let startValue = timer.textContent;
-let timerId = setInterval(() => {
-    startValue--;
-    timer.textContent = startValue;
-    if (startValue == 0) {
-        clearInterval(timerId);
-        alert("Вы победили в конкурсе!");    
+let startValue = timer.textContent.split(':');
+console.log(startValue);
+let secValue = +startValue[2];
+let minValue = +startValue[1];
+let hoursValue = +startValue[0];
+
+function timerCount() {
+    if (secValue !== 0)  {secValue--;        
+    } else {
+        if (minValue !== 0) {
+            secValue = secValue + 59;
+            minValue--; 
+        } else {
+            if (hoursValue !==0) {
+                minValue = minValue + 59;
+                secValue = secValue + 59;
+                hoursValue--;
+            } else {
+                clearInterval(timerId);
+                alert("Вы победили в конкурсе!"); 
+            }
+        }
     }
-}, 1000);
+    // вывод в формате 00:00:00
+    let hoursValueOut = (hoursValue < 10) ? "0" + hoursValue : hoursValue;
+    let minValueOut = (minValue < 10) ? "0" + minValue : minValue;
+    let secValueOut = (secValue < 10) ? "0" + secValue : secValue;
+    
+    timer.textContent = hoursValueOut + ":" + minValueOut + ":" + secValueOut;      
+} 
+
+let timerId = setInterval(timerCount, 1000);
