4df3b28f8ac5499a0f18c96e168e548a145f5661
VladimsKov <vladimskv@yandex.ru>
2022-05-31 07:34:44 +0300

    Решение

---
+++ element-search/popups/task.js
@@ -0,0 +1,12 @@
+ const modalWindow = document.getElementById('modal_main');
+ modalWindow.className = 'modal modal_active';
+ const modalClose = document.querySelectorAll('.modal__close_times');
+ const showSuccess = document.querySelector('.show-success');
+ const modalSuccess = document.getElementById('modal_success');
+ 
+ modalClose[0].onclick = () => modalWindow.className = 'modal';
+ modalClose[1].onclick = () => {
+    modalSuccess.className = 'modal';
+    modalWindow.className = 'modal'; 
+}
+showSuccess.onclick = () => modalSuccess.className =  'modal modal_active';
