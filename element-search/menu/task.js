e5ee02e7aa0752f61d81de2a6d6496884153cbc8
VladimsKov <vladimskv@yandex.ru>
2022-06-01 07:11:59 +0300

    Решение

---
+++ element-search/menu/task.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll('.menu__link');
+const subLinks = document.querySelectorAll('.menu_sub'); 
+const arrLinks = Array.from(links);
+
+function closeOtherLinks (item) {
+    for (let elem of subLinks) {
+        if (elem !== item) {
+            elem.className = 'menu menu_sub';                    
+        }
+    }
+}
+
+arrLinks.forEach((item) => {
+    let parentItem = item.closest('.menu__item');
+    let subItem = parentItem.querySelector('.menu_sub');
+    if (subItem) {
+        item.onclick = () => {
+            subItem.className = 'menu menu_sub menu_active';
+            closeOtherLinks(subItem);            
+            return false;
+        }
+    }    
+});        
