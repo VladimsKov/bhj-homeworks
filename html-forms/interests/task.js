d1e12702e91d1c285f86df62143481177af0d6c6
VladimsKov <vladimskv@yandex.ru>
2022-06-19 21:39:54 +0300

    решение

---
+++ html-forms/interests/task.js
@@ -0,0 +1,21 @@
+const card = document.querySelectorAll('.interests_main > ul > li');
+
+function changeCheck(list) {
+    if (list[0].checked) {
+        for (let i = 1; list.length; i++) {
+            list[i].checked = true;
+        } 
+    } else {
+        for (let i = 1; list.length; i++) {
+            list[i].checked = false;
+        }
+    }
+}
+
+for (let item of card) {
+    let checkBoxList = Array.from(item.querySelectorAll('.interest__check'));
+    checkBoxList.forEach(elem => {
+        elem.checked = false;        
+    });
+    checkBoxList[0].addEventListener('change', function() {changeCheck(checkBoxList)});    
+} 
