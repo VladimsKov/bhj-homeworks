a9f575598ddbb59c75a733e8b5386b6dc00a3d84
VladimsKov <vladimskv@yandex.ru>
2022-06-23 00:24:17 +0300

    решение

---
+++ document-structure/todo/task.js
@@ -0,0 +1,29 @@
+const tasksInput = document.getElementById('task__input');
+const tasksList = document.getElementById('tasks__list');
+const taskAddButton = document.getElementById('tasks__add');
+taskAddButton.setAttribute('type', 'button');
+
+function addTask(taskText) {
+    let task = document.createElement('div');
+    task.className = 'task';
+    task.innerHTML = `<div class="task__title">
+    ${taskText}
+    </div>
+    <a href="#" class="task__remove">&times;</a>` 
+    tasksList.appendChild(task);
+    task.lastChild.addEventListener('click', () => {
+        task.remove();
+    })    
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'Enter' & !!tasksInput.value) {
+        addTask(tasksInput.value);        
+    }
+})
+
+taskAddButton.addEventListener('click', () => {
+    if (!!tasksInput.value) {
+        addTask(tasksInput.value);
+    }
+})
