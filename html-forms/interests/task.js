6938556952c2e6bc13040c442db68cc6c13820e2
VladimsKov <vladimskv@yandex.ru>
2022-06-24 01:03:23 +0300

    исправление

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,11 +2,11 @@ const card = document.querySelectorAll('.interests_main > ul > li');
 
 function changeCheck(list) {
     if (list[0].checked) {
-        for (let i = 1; list.length; i++) {
+        for (let i = 1; i < list.length; i++) {
             list[i].checked = true;
         } 
     } else {
-        for (let i = 1; list.length; i++) {
+        for (let i = 1; i < list.length; i++) {
             list[i].checked = false;
         }
     }
@@ -17,5 +17,7 @@ for (let item of card) {
     checkBoxList.forEach(elem => {
         elem.checked = false;        
     });
-    checkBoxList[0].addEventListener('change', function() {changeCheck(checkBoxList)});    
+    checkBoxList[0].addEventListener('change', function() {
+        changeCheck(checkBoxList)
+    });    
 } 
