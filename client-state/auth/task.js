bb4088d163866813f0d223c506aa93d24ec7fcff
VladimsKov <vladimskv@yandex.ru>
2022-07-01 07:50:31 +0300

    xhr correction

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,25 +1,21 @@
 const signingForm = document.getElementById('signin__form');
 const greating = document.getElementById('welcome');
-const loginField = signingForm.elements.login;
-const passwordField = signingForm.elements.password;
 
 function xhr(method, url, data) {
     const request = new XMLHttpRequest();
     request.open(method, url);
-    request.addEventListener('readystatechange', function() {
-        if (this.readyState === this.DONE && this.status === 200) {
-            let responseData = JSON.parse(this.responseText); 
-            if (responseData.success) {
-                localStorage.setItem('id', responseData.user_id);
-                greating.firstElementChild.textContent = responseData.user_id;
-                signingForm.closest('.signin').classList.remove('signin_active');
-                greating.classList.add('welcome_active');                
-            } else {
-                alert('Неверный логин или пароль');
-                loginField.value = '';
-                passwordField.value = '';
-            }                      
-        }
+    request.responseType ='json';
+    request.addEventListener('load', function() {
+        if (this.response.success) {
+            console.log(this.response);
+            localStorage.setItem('id', this.response.user_id);
+            greating.firstElementChild.textContent = this.response.user_id;
+            signingForm.closest('.signin').classList.remove('signin_active');
+            greating.classList.add('welcome_active');                
+        } else {
+            alert('Неверный логин или пароль');
+            signingForm.reset();                
+        }        
     })
     request.send(data);    
 }
@@ -35,6 +31,6 @@ window.addEventListener('load', () => {
 
 signingForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let signingData = new FormData(signingForm);
+    const signingData = new FormData(signingForm);
     xhr('POST', 'https://netology-slow-rest.herokuapp.com/auth.php', signingData);
 })
